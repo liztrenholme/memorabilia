e51a6505e3c89ed32fafed7651c04fd601007724
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2018-11-17 23:49:27 -0500

    changed to mongoose to connect, require models

--- test/app.js
+++ test/app.js
@@ -1,14 +1,19 @@
 const MongoClient = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
+require('../models');
 
 
 // replace the uri string with your connection string.
-const uri = process.env.MONGO_URI
-MongoClient.connect(uri, function(err, client) {
-   if(err) {
-        console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-   }
-   console.log('Connected...');
-   const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
-});
+// const uri = process.env.MONGO_URI
+var db = mongoose.connect(process.env.MONGO_URI);
+
+// MongoClient.connect(uri, function(err, client) {
+//    if(err) {
+//         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+//    }
+//    console.log('Connected...');
+//    const collection = client.db("test").collection("devices");
+//    // perform actions on the collection object
+//    client.close();
+// });
+
